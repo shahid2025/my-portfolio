c2859f25be1d76e0757d990f210a167deda34ed0
fawadahmadmalik <samtar78625@hotmail.com>
2025-12-19 09:45:33 +0500

    portfolio

---
+++ lib/model/project_model.dart
@@ -0,0 +1,57 @@
+class Project {
+  final String name;
+  final String description;
+  final String image;
+  final String link;
+  Project(this.name, this.description, this.image, this.link);
+}
+
+List<Project> projectList = [
+  Project(
+    'Derma Med',
+    'Derma Med is a modern Flutter-based skincare shopping application designed for clinics and beauty brands. The app showcases premium dermatological products with an elegant UI, including product listings, detailed product pages, pricing, quantity selection, and a smooth add-to-cart experience. Users can browse brands, view product descriptions, and manage their shopping bag seamlessly, creating a professional and user-friendly digital skincare store.',
+    'assets/images/coffee.png',
+    'https://github.com/havetech23/DermaMed_App',
+  ),
+  Project(
+    'Renticity',
+   'A Flutter-based peer-to-peer rental marketplace app with category browsing, location-based listings, item details, and direct owner communication',
+    'assets/images/car.png',
+    'https://github.com/havetech23/rent_market_place',
+  ),
+  Project(
+      'Nusaeid',
+      'Nusaeid is a Flutter-based home services app that allows users to easily book household services such as cleaning, plumbing, and electrical work with scheduled date and time selection.',
+      'assets/images/alarm.jpg',
+      'https://github.com/shahid2025/nusaeid'),
+  Project(
+      'IAdashCam',
+      'IAdashCam is a cutting-edge dashcam application designed for intelligent vehicle monitoring and data management. It provides real-time video recording, GPS tracking, and seamless integration with mobile devices for enhanced safety and analytics. The app also supports advanced features like clip management, memory optimization, and secure data uploads for comprehensive fleet or personal vehicle oversight,',
+          'assets/images/cui.png',
+      'https://github.com/havetech23/IAdashCam'),
+  Project(
+      'YBM Business Application',
+      'The app provides search, bookmark, and tutorial features for businesses. Business entries include name, rating, and descriptive text (placeholder Lorem Ipsum).The design uses a blue‑white theme with card‑style listings and action buttons.',
+      'assets/images/player.png',
+      'https://github.com/Tayyabi/YBM-User-App'),
+  // Project(
+  //     'Food Recipe App',
+  //     'This repository contains a Flutter implementation of a Food Recipe app with a captivating introduction section and impressive animations. It also features a signup and signin page with animations, elegantly presented on a bottom sheet.',
+  //     'assets/images/recipe.png',
+  //     'https://github.com/Hamad-Anwar/Food-Recipe-App-Flutter'),
+  Project(
+      'Top Bantz app',
+      "Top Bantz is a slick football app that lets you track La Liga standings, manage your profile, and earn coins while staying in the game.",
+      'assets/images/task.png',
+      'https://github.com/Tayyabi/topBantz'),
+  Project(
+      'Hastras islamic Application',
+      'Hastras offers Islamic charitable services including meal distributions, Zabiha (sacrifice), “House in Paradise” mosque donations, Aqeeqah for newborns, and Iftar subscriptions, all linked to Islamic practices and rewards',
+      'assets/images/chat.png',
+      'https://github.com/havetech23/hastras'),
+  Project(
+      'US Booth App',
+      'US Booths creates custom booth solutions that elevate environments from high-end restaurants to luxury residential spaces.',
+      'assets/images/doctor.png',
+      'https://github.com/umairali435/usboothApp'),
+];
